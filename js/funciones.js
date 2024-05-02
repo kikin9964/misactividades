de7acc63ab9cfb0a69c635c709cabf53c0082bb9
kikin9964 <161506961+kikin9964@users.noreply.github.com>
2024-05-02 12:24:23 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,17 @@
+$(document).ready(function()
+{
+
+	$(".playlist").audioControls(
+	{
+		autoPlay:false,
+		timer:'increment',
+		onAudioChange:function(datos)
+		{
+			$(".nombremp3").text(datos.title);
+		}
+
+	});
+
+
+
+});
